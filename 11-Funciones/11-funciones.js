b11a00cf57186642291b17cc8dc71e3df91bc4ed
BParreno <biparreno@sudamericano.edu.ec>
2024-06-04 10:41:36 -0500

    Commit 2 (Clases Previas)

---
+++ 11-Funciones/11-funciones.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function sumar(a, b) {
+let result = a + b;
+return (result);
+};
+alert (sumar(3,2));
+
---
+++ 22- Temperatura/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let a= prompt("Ingrese la temperatura en grados celcius que desea convertir");
+let temperature =(a,b) => a*b;
+let result = temperature (a,33.8)
+alert(result + "grados Fahrenheit");
+
+
