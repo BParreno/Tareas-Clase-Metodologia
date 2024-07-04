2792060f994b4e5b93f64a61ac425e1fbda6cef8
BParreno <biparreno@sudamericano.edu.ec>
2024-07-04 10:57:16 -0500

    Commit 3 36-Ejercicios array methods

--- 36-Ejercicios array methods/36-Ejercicios array methods.js
+++ 36-Ejercicios array methods/36-Ejercicios array methods.js
@@ -1,5 +1,21 @@
 "use strict";
 
+//Ejercicio #1
+let students = ["Juan", "Jose", "Pedro", "Maria", "Alejandra"];
+
+function searchName(namesList, wantedName) {
+    if (namesList.includes(wantedName)) {
+        alert("Si se encuentra");
+    } else {
+        alert("No se encuentra"); 
+    }
+}
+
+let wantedName = "Leonard";
+searchName(students, wantedName);
+
+
+//Ejercicio #2
 let array = 'La programación web es fundamental para el progreso';
 
 function wordQuantity(array) {
@@ -9,3 +25,19 @@ function wordQuantity(array) {
 
 let quantity = wordQuantity(array);
 alert(`The phrase "${array}" has ${quantity} words.`);
+
+
+//Ejercicio #3
+
+function threeLetterWords(array) {
+    let words = array.split(" ");
+    
+    let wordsOfThree = words.filter(function(word) {
+        return word.length > 3;
+    });
+    
+    return wordsOfThree;
+}
+
+let result = threeLetterWords(array);
+alert(result);
